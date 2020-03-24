37526d93b402ff58a0cb9d26f05f57053773d05b
Guilherme Henrique <guilherhenri12@gmail.com>
2020-03-24 11:48:26 -0300

    Feature: IncidentController

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,13 @@ const express = require('express');
 const routes = express.Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
